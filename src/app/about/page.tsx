a433f865bc2293c8cae876daaac59ab1b68ed498
victorkane <victorkane@gmail.com>
2023-10-18 11:20:26 -0300

    build: install and configure NextUI with dependencies, and test with dark mode and button on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,11 @@
-import React from "react"
+"use client"
+import { Button, NextUIProvider } from "@nextui-org/react"
 
 export default function About() {
-  return <div>About</div>
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      <h2>About</h2>
+      <Button>Click me!</Button>
+    </main>
+  )
 }
